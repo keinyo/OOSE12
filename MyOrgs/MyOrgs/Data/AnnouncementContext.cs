755af214e936e1682ae429338fc698e177ac797b
keinyo <18519475+keinyo@users.noreply.github.com>
2021-03-01 02:35:45 -0600

    Added tables needed for future functionality and updated readme

--- MyOrgs/MyOrgs/Data/AnnouncementContext.cs
+++ MyOrgs/MyOrgs/Data/AnnouncementContext.cs
@@ -1,4 +1,5 @@
 ﻿//Resources used: https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app
+//Name should be changed to be less specific, as this is the database everything but user accounts use
 using Microsoft.EntityFrameworkCore;
 using MyOrgs.Models;
 
---
+++ MyOrgs/MyOrgs/Data/Migrations/20210224000550_na.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyOrgs.Data.Migrations
+{
+    public partial class na : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
